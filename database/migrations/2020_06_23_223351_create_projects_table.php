7483472ff2cf4b179f6110fc39cfc9c6e1ed0c5e
Karim Mohamed <komicho1996@gmail.com>
2020-06-24 00:36:45 +0200

    Create project model

---
+++ database/migrations/2020_06_23_223351_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('guid')->unique();
+            $table->string('public_key', 16);
+            $table->string('secret_key', 64);
+            $table->json('artisan_orders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
