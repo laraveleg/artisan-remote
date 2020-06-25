c20e2fe063cd02c7c74a6730ef1bbe902740aa58
Karim Mohamed <komicho1996@gmail.com>
2020-06-25 19:29:37 +0200

    Create new project by vue.js

--- resources/js/components/Projets/ProjetsIndexComponent.vue
+++ resources/js/components/Projets/ProjetsIndexComponent.vue
@@ -1,165 +1,98 @@
 <template>
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Last commits</h3>
-        </div>
-        <div class="list list-row list-hoverable">
-            <div class="list-item">
-                <div><span class="badge bg-red"></span></div>
-                <a href="#">
-                    <span class="avatar" style="background-image: url(./static/avatars/000m.jpg)"></span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Paweł Kuna</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Change deprecated html tags to text decoration
-                        classes (#29604)</small>
+    <div>
+
+
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Projects list</h3>
+                <div class="card-options">
+                    <a href="javascript:false" class="btn btn-primary btn-sm" @click="addNewProject">Create new project</a>
                 </div>
-                <a href="#" class="list-item-actions"><svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
             </div>
-            <div class="list-item">
-                <div><span class="badge"></span></div>
-                <a href="#">
-                    <span class="avatar">JL</span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Jeffie Lewzey</a>
-                    <small class="d-block text-muted text-truncate mt-n1">justify-content:between ⇒
-                        justify-content:space-between (#29734)</small>
+            <div class="list list-row list-hoverable">
+
+                <div class="list-item">
+                    <div><span class="badge"></span></div>
+                    <a href="#">
+                        <span class="avatar">EP</span>
+                    </a>
+                    <div class="text-truncate">
+                        <a href="#" class="text-body d-block">Egan Poetz</a>
+                        <small class="d-block text-muted text-truncate mt-n1">Copywriting edits</small>
+                    </div>
                 </div>
-                <a href="#" class="list-item-actions show"><svg xmlns="http://www.w3.org/2000/svg"
-                        class="icon text-yellow" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
-            </div>
-            <div class="list-item">
-                <div><span class="badge bg-"></span></div>
-                <a href="#">
-                    <span class="avatar" style="background-image: url(./static/avatars/002m.jpg)"></span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Mallory Hulme</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Update change-version.js (#29736)</small>
-                </div>
-                <a href="#" class="list-item-actions"><svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
-            </div>
-            <div class="list-item">
-                <div><span class="badge bg-green"></span></div>
-                <a href="#">
-                    <span class="avatar" style="background-image: url(./static/avatars/003m.jpg)"></span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Dunn Slane</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Regenerate package-lock.json (#29730)</small>
-                </div>
-                <a href="#" class="list-item-actions"><svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
-            </div>
-            <div class="list-item">
-                <div><span class="badge bg-red"></span></div>
-                <a href="#">
-                    <span class="avatar" style="background-image: url(./static/avatars/000f.jpg)"></span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Emmy Levet</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Some minor text tweaks</small>
-                </div>
-                <a href="#" class="list-item-actions show"><svg xmlns="http://www.w3.org/2000/svg"
-                        class="icon text-yellow" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
-            </div>
-            <div class="list-item">
-                <div><span class="badge bg-yellow"></span></div>
-                <a href="#">
-                    <span class="avatar" style="background-image: url(./static/avatars/001f.jpg)"></span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Maryjo Lebarree</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Link to versioned docs</small>
-                </div>
-                <a href="#" class="list-item-actions"><svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
-            </div>
-            <div class="list-item">
-                <div><span class="badge"></span></div>
-                <a href="#">
-                    <span class="avatar">EP</span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Egan Poetz</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Copywriting edits</small>
-                </div>
-                <a href="#" class="list-item-actions"><svg xmlns="http://www.w3.org/2000/svg" class="icon text-muted"
-                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                        stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
-            </div>
-            <div class="list-item">
-                <div><span class="badge bg-yellow"></span></div>
-                <a href="#">
-                    <span class="avatar" style="background-image: url(./static/avatars/002f.jpg)"></span>
-                </a>
-                <div class="text-truncate">
-                    <a href="#" class="text-body d-block">Kellie Skingley</a>
-                    <small class="d-block text-muted text-truncate mt-n1">Enable RFS for font resizing</small>
-                </div>
-                <a href="#" class="list-item-actions show"><svg xmlns="http://www.w3.org/2000/svg"
-                        class="icon text-yellow" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" />
-                        <path
-                            d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z" />
-                        </svg>
-                </a>
+
             </div>
         </div>
+
+
+        <div class="modal modal-blur fade" id="model-add-new-project" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">New project</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label">Name</label>
+                            <input type="text" class="form-control" name="example-text-input" placeholder="Your report name" v-model="name">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#" class="btn btn-link link-secondary" data-dismiss="modal">
+                            Cancel
+                        </a>
+                        <a href="javascript:false" class="btn btn-primary ml-auto" @click="saveNewProject">
+                            Create new project
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-    
+
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
+    // export default {
+    //     mounted() {
+    //         console.log('Component mounted.')
+    //     }
+    // }
+
+</script>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {
+        Component,
+        Prop
+    } from 'vue-property-decorator';
+    import axios from 'axios';
+
+    @Component
+    export default class ProjetsIndexComponent extends Vue
+    {
+        name:string = '';
+
+        addNewProject()
+        {
+            console.log((<any>window).axios);
+            $('#model-add-new-project').modal();
         }
+
+        saveNewProject()
+        {
+            axios.post('/api/projects/store', {
+                name: this.name,
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch(error => console.error(error));
+        }
+
     }
 
 </script>
